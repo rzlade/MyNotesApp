3d629af738423c6a4c741211cdb8f60212055099
rzlade <khoerulikhsan.r@gmail.com>
2020-02-04 20:55:55 +0700

    First Version NotesApp

---
+++ app/src/main/java/com/example/mynotesapp/helper/MappingHelper.kt
@@ -0,0 +1,20 @@
+package com.example.mynotesapp.helper
+
+import android.database.Cursor
+import com.example.mynotesapp.db.DatabaseContract
+import com.example.mynotesapp.entity.Note
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(notesCursor: Cursor): ArrayList<Note>{
+        val notesList = ArrayList<Note>()
+        notesCursor.moveToFirst()
+        while (notesCursor.moveToNext()){
+            val id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+            val title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+            val description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+            val date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+        }
+        return notesList
+    }
+}
